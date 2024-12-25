7bc65216606b4d417eefdda534ec0aec38e6b8f7
vaibhav100995 <vaibhav100995@gmail.com>
2024-12-25 18:36:16 +0530

    src code

---
+++ src/test/java/com/configserver/configservercheck/ConfigservercheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configserver.configservercheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigservercheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
